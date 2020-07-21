0863726c2820ddb0f5654ab62cc1bf2992a76157
narnhada <kbh91122@naver.com>
2020-07-21 21:40:34 +0900

    Updates

---
+++ precache-manifest.67dc7995d4076a85b617efb3054facec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902ec9f632fac5ce373977afd0ba4c4f",
+    "url": "/todo_app2/index.html"
+  },
+  {
+    "revision": "e6d97f69b48e4d5017a9",
+    "url": "/todo_app2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9076b9067e38ee516aba",
+    "url": "/todo_app2/static/js/2.9ecca6fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_app2/static/js/2.9ecca6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d97f69b48e4d5017a9",
+    "url": "/todo_app2/static/js/main.a39ff7fc.chunk.js"
+  },
+  {
+    "revision": "623e19a301627ae1547b",
+    "url": "/todo_app2/static/js/runtime-main.291483ae.js"
+  }
+]);
